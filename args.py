770ee4e8445b1f8dd1ed71bb736186efe4f121bc
njugunagrace <njugunagraceewambui@gamil.com>
2023-04-14 20:56:11 +0300

    changes

--- args.py
+++ args.py
@@ -2,14 +2,21 @@
 # positional arguments format and concatenates them into a single string
 
 def concatenate_args(*words):
+    answer = " "
     for word in words:
-        print(f"My name is {word}")
+        answer +=  (f"My name is {word}")
+    return answer        
+        
+        
     
 #  A function named concatenate_kwargs that takes any number of string arguments 
 #  in keyword arguments  format and concatenates them into a single string   
     
 def concatenate_kwargs(**kwargs):
+    result = (" ")
     for key,value in kwargs.items():
-                print(f"{key}:{value}")
+        result+= (f"{key,value}")
+    return result
+        
 
         
